e0bd1bbe964b411edd1aef8962821c1fb4a58de6
viwivi-v <tammaipanyaon@gmail.com>
2025-09-13 14:03:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// This file is ready for you to add JavaScript code to make your portfolio more interactive.
+// For example, you could add code for:
+// - A modal pop-up when an image is clicked
+// - Filtering projects by category
+// - Animate elements on scroll
